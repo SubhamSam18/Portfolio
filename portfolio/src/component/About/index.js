de908dab05ad8b4a2f4f142654b1ff02c31338ba
Subham Samanta <subhamsamanta189@gmail.com>
2022-12-15 17:41:55 +0530

    Fixed cube rotation

--- portfolio/src/component/About/index.js
+++ portfolio/src/component/About/index.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBootstrap,faCss3, faHtml5, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
 
 import {SiMongodb} from 'react-icons/si';
-
+import Typical from 'react-typical'
 const About = () => {
   console.log("Hello About!");
   return (
@@ -13,21 +13,30 @@ const About = () => {
         <h1><br />About</h1>
       </div>
         <div className='about-body'>
-          <p>
+        <Typical
+            loop={Infinity}
+            steps={[
+               `Hi, I'm a web developer with a focus on the MERN stack, but still 
+               exploring other technologies and frameworks that catch my interest!
+               If you're looking for a developer to add to your team, 
+               I'd love to hear from you!`,
+              200,
+            ]} />
+          {/* <p>
             Hi, I'm a web developer with a focus on the MERN stack, but still <br></br><br></br>exploring other technologies and frameworks that catch my interest!<br></br><br></br> If you're looking for a developer to add to your team, <br></br><br></br>I'd love to hear from you!
-          </p>
+          </p> */}
         </div>
         
         <div className='stage-cube'>
           <div className='cube-spinner'>
             <div className='face1'>
-              <FontAwesomeIcon icon={faReact} color="red" />
+              <FontAwesomeIcon icon={faCss3} color="red" />
             </div>
             <div className='face2'>
               <FontAwesomeIcon icon={faHtml5} color="red" />
             </div>
             <div className='face3'>
-              <FontAwesomeIcon icon={faCss3} color="red" />
+              <FontAwesomeIcon icon={faReact} color="red" />
             </div>
             <div className='face4'>
               <FontAwesomeIcon icon={faNodeJs} color="red" />
